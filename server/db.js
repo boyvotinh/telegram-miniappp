0cd67b1dc496aff2d619cb75eeb644627e7b3b2b
boyvotinh <LongVTQ.PTIT@gmail.com>
2025-04-25 16:30:19 +0700

    code17

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2');
-
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',       
-  password: '14012005LongDZ@#$', 
-  database: 'TaskManager' 
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
-
 connection.connect((err) => {
   if (err) {
     console.error('❌ Lỗi kết nối MySQL:', err);
