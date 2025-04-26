22dfe3f8ca49aa4f58f1d57d79959d78bac2fe7f
boyvotinh <LongVTQ.PTIT@gmail.com>
2025-04-27 00:24:30 +0700

    fontend22

--- server/bot.js
+++ server/bot.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const db = require('./db');
+const TelegramBot = require('node-telegram-bot-api');
 
 app.use(bodyParser.json());
 
